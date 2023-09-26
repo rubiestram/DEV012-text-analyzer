c9122b7dbd2c72ace748d73ecaa640564d86b83a
Rubi <rubiestram@gmail.com>
2023-09-26 08:35:59 -0600

    Inicialización de metricas en cero y botón de limpiar funcionando

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+// Textarea
+const textarea = document.querySelector('textarea[name="user-input"]');
+// Evento Input
+textarea.addEventListener("input", (e) => {
+  console.log(e) //probando evento
+});
+
+// Boton de Eliminar
+const resetButton = document.getElementById("reset-button");
+//Evento Click
+resetButton.addEventListener('click', () => {
+  textarea.value = '';
+});
 
 
+
+// Inicializamos las metricas en 0
+const wordCount = document.querySelector('.wordcount');
+const charactercount = document.querySelector('.charactercount')
+const characternospacescount = document.querySelector('.characternospacescount')
+const numbercount = document.querySelector('.numbercount')
+const numbersum = document.querySelector('.numbersum')
+const wordlengthaverage = document.querySelector('.wordlengthaverage')
+
+wordCount.textContent += " 0";
+charactercount.textContent += " 0";
+characternospacescount.textContent += " 0";
+numbercount.textContent += " 0";
+numbersum.textContent += " 0";
+wordlengthaverage.textContent += " 0";
